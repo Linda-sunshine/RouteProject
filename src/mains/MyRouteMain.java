1e52b1eeb295870171bd7703b6bcefa1f1cc7e7d
Linda-sunshine <gonglin1988@gmail.com>
2017-03-22 10:25:40 -0400

    test

--- src/mains/MyRouteMain.java
+++ src/mains/MyRouteMain.java
@@ -3,7 +3,6 @@ package mains;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import opennlp.tools.util.InvalidFormatException;
-import structures._User;
 import Analyzer.BinaryRouteAnalyzer;
 import Classifier.supervised.GlobalSVM;
 import Classifier.supervised.modelAdaptation.MultiTaskSVM;
